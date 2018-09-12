3a21178624f4e408c516b7114127de93d7b714ee
Tyson <twillis@relevantinc.com>
2018-09-12 06:49:41 -0600

    Dev tool initial commit

---
+++ lib/twillis_view_tool.rb
@@ -0,0 +1,6 @@
+require "twillis_view_tool/version"
+require "twillis_view_tool/renderer"
+
+module TwillisViewTool
+  # Your code goes here...
+end
---
+++ lib/twillis_view_tool/version.rb
@@ -0,0 +1,3 @@
+module TwillisViewTool
+  VERSION = "0.1.0"
+end
